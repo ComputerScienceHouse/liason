a7d17f0b4fa92f41887e92c0a7ca54747098319e
Tyler Allen <tyler@tallen.me>
2025-04-30 02:55:38 +0000

    liason v1

---
+++ db.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"log"
+	"os"
+	"time"
+)
+
+var Client = Connect()
+
+var db = os.Getenv("VOTE_MONGO_DB")
+var dbUri = os.Getenv("VOTE_MONGO_URI")
+
+func Connect() *mongo.Client {
+
+	ctx, cancel := context.WithTimeout(context.TODO(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(dbUri))
+	if err != nil {
+		log.Panicln(`error connecting to database`, err)
+	}
+
+	if err = client.Ping(ctx, readpref.Primary()); err != nil {
+		log.Panicln("error pinging database", err)
+	}
+
+	log.Println("connected to mongodb")
+
+	return client
+}
+
+type Relation struct {
+	EboardTS string `bson:eboardTS`
+	ClientID string `bson:clientID`
+	ClientTS string `bson:clientTS`
+}
+
+func LoadRelations() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	cursor, err := Client.Database(db).Collection("relations").Find(ctx, bson.D{})
+	if err != nil {
+		fmt.Println("HELP", err)
+		return
+	}
+	relations := make([]Relation, 0)
+
+	err = cursor.All(ctx, &relations)
+	if err != nil {
+		fmt.Println("HELP", err)
+		return
+	}
+
+	for _, relation := range relations {
+		clientString := relation.ClientID + ":" + relation.ClientTS
+		EboardRelations[relation.EboardTS] = clientString
+		ClientRelations[clientString] = relation.EboardTS
+	}
+
+}
+
+func MakeRelation(eboardTS, clientID, clientTS string) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	newRelation := Relation{EboardTS: eboardTS, ClientTS: clientTS, ClientID: clientID}
+
+	_, err := Client.Database(db).Collection("relations").InsertOne(ctx, newRelation)
+	if err != nil {
+		return
+	}
+}
---
+++ main.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"fmt"
+	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/slackevents"
+	"github.com/slack-go/slack/socketmode"
+	"os"
+	"strings"
+)
+
+var LiasonChannel = os.Getenv("LIASON_CHANNEL_ID")
+
+// [eboard] client
+var EboardRelations = make(map[string]string)
+
+// [client] eboard
+var ClientRelations = make(map[string]string)
+
+const ThisUserID = "U08PJ68RKLN"
+
+func main() {
+	if LiasonChannel == "" {
+		fmt.Fprintf(os.Stderr, "LIASON_CHANNEL_ID must be set.\n")
+		os.Exit(1)
+	}
+
+	//get and verify app token (TBH this might be removable)
+	appToken := os.Getenv("SLACK_APP_TOKEN")
+	if appToken == "" {
+		fmt.Fprintf(os.Stderr, "SLACK_APP_TOKEN must be set.\n")
+		os.Exit(1)
+	}
+	if !strings.HasPrefix(appToken, "xapp-") {
+		fmt.Fprintf(os.Stderr, "SLACK_APP_TOKEN must have the prefix \"xapp-\".")
+	}
+
+	//get and verify bot token
+	botToken := os.Getenv("SLACK_BOT_TOKEN")
+	if botToken == "" {
+		fmt.Fprintf(os.Stderr, "SLACK_BOT_TOKEN must be set.\n")
+		os.Exit(1)
+	}
+	if !strings.HasPrefix(botToken, "xoxb-") {
+		fmt.Fprintf(os.Stderr, "SLACK_BOT_TOKEN must have the prefix \"xoxb-\".")
+	}
+
+	//load API accordingly
+	api := slack.New(botToken, slack.OptionAppLevelToken(appToken))
+	client := socketmode.New(api) //,
+	//socketmode.OptionDebug(true),
+	//socketmode.OptionLog(log.New(os.Stdout, "socketmode: ", log.Lshortfile|log.LstdFlags)))
+	socketHandler := socketmode.NewSocketmodeHandler(client)
+
+	//handle Slack normal endpoints
+	socketHandler.Handle(socketmode.EventTypeConnecting, func(event *socketmode.Event, client *socketmode.Client) {
+		fmt.Println("Connecting to Slack")
+	})
+	socketHandler.Handle(socketmode.EventTypeConnectionError, func(event *socketmode.Event, client *socketmode.Client) {
+		fmt.Println("Connection Failed. Trying Again?")
+	})
+	socketHandler.Handle(socketmode.EventTypeConnected, func(event *socketmode.Event, client *socketmode.Client) {
+		fmt.Println("Connected to Slack via Socket Mode.")
+	})
+	socketHandler.Handle(socketmode.EventTypeHello, func(event *socketmode.Event, client *socketmode.Client) {
+		fmt.Println("Hello message received.")
+	})
+
+	LoadRelations()
+
+	socketHandler.Handle(socketmode.EventTypeEventsAPI, handleEvents)
+
+	socketHandler.HandleSlashCommand("/start", startCommand)
+
+	socketHandler.RunEventLoop()
+
+}
+
+func handleEvents(evt *socketmode.Event, client *socketmode.Client) {
+	eventsAPIEvent, ok := evt.Data.(slackevents.EventsAPIEvent)
+	if !ok {
+		return
+	}
+	client.Ack(*evt.Request)
+
+	if eventsAPIEvent.Type != slackevents.CallbackEvent {
+		return
+	}
+	if eventsAPIEvent.InnerEvent.Type != "message" {
+		return
+	}
+	event := eventsAPIEvent.InnerEvent.Data.(*slackevents.MessageEvent)
+	if event.User == ThisUserID || event.User == "" {
+		return
+	}
+	switch event.ChannelType {
+	case "group":
+		handleEboard(event, client)
+	case "im":
+		handleDM(event, client)
+	default:
+		return
+	}
+}
+
+func handleEboard(event *slackevents.MessageEvent, client *socketmode.Client) {
+	if event.Channel != LiasonChannel {
+		fmt.Println("Messaged from a different channel?")
+		client.LeaveConversation(event.Channel)
+		return
+	}
+	if event.ThreadTimeStamp == "" {
+		return
+	}
+	eboardTS := EboardRelations[event.ThreadTimeStamp]
+	if eboardTS == "" {
+		return
+	}
+	sendData := strings.Split(eboardTS, ":")
+	client.SendMessage(sendData[0], slack.MsgOptionUsername("EBoard"), slack.MsgOptionTS(sendData[1]), slack.MsgOptionText(event.Text, false))
+}
+
+func handleDM(event *slackevents.MessageEvent, client *socketmode.Client) {
+	if event.ThreadTimeStamp == "" {
+		client.SendMessage(event.Channel, slack.MsgOptionText("Want to start a new conversation? /start", false))
+		return
+	}
+	sendTS := ClientRelations[event.Channel+":"+event.ThreadTimeStamp]
+	if sendTS == "" {
+		return
+	}
+	client.SendMessage(LiasonChannel, slack.MsgOptionUsername("Concerned Member"), slack.MsgOptionTS(sendTS), slack.MsgOptionText(event.Text, false))
+	//TODO: maybe hash threadTS for privacy?
+}
+
+func startCommand(evt *socketmode.Event, client *socketmode.Client) {
+	cmd, ok := evt.Data.(slack.SlashCommand)
+	if !ok {
+		return
+	}
+	client.Ack(*evt.Request)
+
+	if cmd.ChannelName != "directmessage" {
+		return
+	}
+
+	_, dmTS, _, _ := client.SendMessage(cmd.ChannelID, slack.MsgOptionText("Conversation :thread: here <@"+cmd.UserID+">", false))
+	_, ebTS, _, _ := client.SendMessage(LiasonChannel, slack.MsgOptionText("Conversation :thread: here", false))
+	clientStr := cmd.ChannelID + ":" + dmTS
+	EboardRelations[ebTS] = clientStr
+	ClientRelations[clientStr] = ebTS
+	MakeRelation(ebTS, cmd.ChannelID, dmTS)
+}
